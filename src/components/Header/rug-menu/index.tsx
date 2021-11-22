1ed358b48be2d0bfb44c0691017bfbf7bed24fe2
Jacob <82294478+camdengrieh@users.noreply.github.com>
2021-11-22 19:11:47 +0000

    Change Buy Link

--- src/components/Header/rug-menu/index.tsx
+++ src/components/Header/rug-menu/index.tsx
@@ -60,7 +60,7 @@ function TimeMenu() {
             <div className="tooltip">
               <Link
                 className="tooltip-item"
-                href={`https://www.traderjoexyz.com/#/trade?inputCurrency=&outputCurrency=${RUG_ADDRESS}`}
+                href={`https://traderjoexyz.com/#/trade?inputCurrency=0x130966628846bfd36ff31a822705796e8cb8c18d&outputCurrency=0xb8ef3a190b68175000b74b4160d325fd5024760e`}
                 target="_blank"
               >
                 <p>Buy on Trader Joe</p>

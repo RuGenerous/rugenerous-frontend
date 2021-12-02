21af7a6c37f4d61f78d172b385d4ee738e9459b7
Jacob <82294478+camdengrieh@users.noreply.github.com>
2021-12-02 10:51:21 +0000

    Add bonds

--- src/helpers/token-price.ts
+++ src/helpers/token-price.ts
@@ -4,12 +4,13 @@ const cache: { [key: string]: number } = {};
 
 export const loadTokenPrices = async () => {
   const url =
-    "https://api.coingecko.com/api/v3/simple/price?ids=avalanche-2,benqi,olympus,magic-internet-money&vs_currencies=usd";
+    "https://api.coingecko.com/api/v3/simple/price?ids=avalanche-2,benqi,usd-coin,tether,olympus,magic-internet-money&vs_currencies=usd";
   const { data } = await axios.get(url);
 
   cache["AVAX"] = data["avalanche-2"].usd;
   cache["MIM"] = data["magic-internet-money"].usd;
-  cache["USDC"] = data["magic-internet-money"].usd;
+  cache["USDC"] = data["usd-coin"].usd;
+  cache["USDT"] = data["tether"].usd;
   cache["OHM"] = data["olympus"].usd;
   cache["QI"] = data["benqi"].usd;
 };

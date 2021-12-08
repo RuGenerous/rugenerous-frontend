dcd9f04404cb4b628dcf92bc310eb1712da6482b
vitznau <vitznau@x.noop>
2021-12-07 20:31:45 -0500

    update footer copy on staking page

--- src/views/Stake/index.tsx
+++ src/views/Stake/index.tsx
@@ -421,10 +421,13 @@ function Stake() {
                     <div className="stake-card-action-help-text">
                       <br />
                       <p>
-                        Note: There is a 2 epoch warm-up staking period, where users must be staked for more than 2
-                        epoch's /rebases before claiming any rebase rewards. When 2 epochs has elapsed your staked
-                        balance can be claimed from the warm up contract and you will automatically receive the rebase
-                        rewards thereafter.
+                        Please Note: there is a two-epoch warm-up period when staking. One epoch is eight hours (one rebase window).
+                        During warm-up, your staked tokens are held by the warm-up contract. Exiting the warm-up early will return your
+                        original deposit to your wallet.
+
+                        Your staked tokens and their accrued rebase rewards will be available to claim at the start of the third epoch 
+                        after you initially staked. Once claimed, the tokens move to your staked token balance, where they will continue
+                        to earn rebase rewards and can be unstaked at any time without penalty.
                       </p>
                     </div>
                   </div>

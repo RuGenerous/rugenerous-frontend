069cf72302fd5ea2ecfe0627f3c311e11006cc64
vitznau <vitznau@x.noop>
2021-12-16 14:52:43 -0500

    Update pair to RUG/USDC

--- src/components/Header/rug-menu/index.tsx
+++ src/components/Header/rug-menu/index.tsx
@@ -60,10 +60,10 @@ function TimeMenu() {
             <div className="tooltip">
               <Link
                 className="tooltip-item"
-                href={`https://swap.rug.farm/#/trade?inputCurrency=0x130966628846bfd36ff31a822705796e8cb8c18d&outputCurrency=0xb8ef3a190b68175000b74b4160d325fd5024760e`}
+                href={`https://swap.rug.farm/#/trade?inputCurrency=0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664&outputCurrency=0xb8ef3a190b68175000b74b4160d325fd5024760e`}
                 target="_blank"
               >
-                <p>Buy on RUG Exchange</p>
+                <p>Buy on RugSwap</p>
               </Link>
               <Link
                 className="tooltip-item"

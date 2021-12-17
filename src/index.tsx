496f263b34b3ec18c6a65a429169b9c508997b56
John Castorina <john.castorina@carvana.com>
2021-12-16 17:34:02 -0700

    instantiate react query client at root level

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,30 @@ import { Provider } from "react-redux";
 import { Web3ContextProvider } from "./hooks";
 import { SnackbarProvider } from "notistack";
 import SnackMessage from "./components/Messages/snackbar";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <SnackbarProvider
-    maxSnack={4}
-    anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "right",
-    }}
-    content={(key, message: string) => <SnackMessage id={key} message={JSON.parse(message)} />}
-    autoHideDuration={10000}
-  >
-    <Provider store={store}>
-      <Web3ContextProvider>
-        <Root />
-      </Web3ContextProvider>
-    </Provider>
-  </SnackbarProvider>,
+  <QueryClientProvider client={queryClient}>
+    <SnackbarProvider
+      maxSnack={4}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      content={(key, message: string) => <SnackMessage id={key} message={JSON.parse(message)} />}
+      autoHideDuration={10000}
+    >
+      <Provider store={store}>
+        <Web3ContextProvider>
+          <Root />
+        </Web3ContextProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Provider>
+    </SnackbarProvider>
+  </QueryClientProvider>,
   document.getElementById("root"),
 );

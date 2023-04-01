e8bdec88f12eec6d5732cc1f55fbfa3944685819
pmjanus <97489909+pmjanus@users.noreply.github.com>
2023-04-01 17:21:45 -0500

    use api key

--- src/helpers/geolocation.ts
+++ src/helpers/geolocation.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const API_KEY = "7827544add0b453fb60303fade35beec"; // Replace with your ipapi API key
+
 export async function isUSUser() {
   try {
-    const response = await axios.get(`https://api.ipgeolocation.io/ipgeo`);
+    const response = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}`);
+    console.log(response);
     return response.data.country_code2 === "US";
   } catch (error) {
     console.error("Error determining user location:", error);
